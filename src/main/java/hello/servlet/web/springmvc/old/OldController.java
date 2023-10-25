8e7c7c1f2fd8495eb68a6e0b2993c8660bc14e96
dev_one <micool1030@gmail.com>
2023-10-25 23:21:56 +0900

    # OldController 테스트

---
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,28 @@
+package hello.servlet.web.springmvc.old;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * OldController
+ * <pre>
+ * Describe here
+ * </pre>
+ *
+ * @version 1.0,
+ */
+// 스프링 빈의 이름을 아래와 같이 세팅
+// 0순위 : @RequestMapping > 1순위 : 스프링 빈의 이름으로 핸들러를 찾음
+@Component("/spring/old-controller")
+public class OldController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return null;
+    }
+}
